f165c5af7a371b4cc7b6a5e2af53ea001ac2146e
vaheminasyan2 <vaheminasyan2@gmail.com>
2020-02-09 22:08:08 -0800

    make it modular

--- make-it-modular.js
+++ make-it-modular.js
@@ -2,15 +2,11 @@ const mymodule = require("./mymodule.js");
 var directory = process.argv[2];
 var fileExt = process.argv[3];
 
-mymodule(directory, fileExt)
-
-fs.readdir(directory, (err, data) => {
-  if (err) throw err;
-
-  for (var i = 0; i < data.length; i++) {
-    if (path.extname(data[i]) === "." + fileExt) {
-      console.log(data[i]);
-    }
+mymodule(directory, fileExt, function(err, list) {
+  if (err) {
+    return console.error("there was an error:", err);
   }
+  list.forEach(function(file) {
+    console.log(file);
+  });
 });
-
--- mymodule.js
+++ mymodule.js
@@ -1,22 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = function bar (callback) {
-    foo(function (err, data) {
-      if (err) { return callback(err) } // early return
+module.exports = function(directory, fileExt, callback) {
+  fs.readdir(directory, function(err, list) {
+    if (err) {
+      return callback(err);
+    }
 
-      // ... no error, continue doing cool things with `data`
-        fs.readdir(directory, (err,data) =>{
-            if (err) throw err;
+    list = list.filter(function(file) {
+      return path.extname(file) === "." + fileExt;
+    });
 
-            for (var i = 0; i< data.length; i++) {
-                if (path.extname(data[i]) === "." + fileExt) {
-                    console
-                }
-            }
-        })
-      // all went well, call callback with `null` for the error argument
-
-      callback(null, data)
-    })
-  }
+    callback(null, list);
+  });
+};
