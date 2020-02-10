8392cc5fc7967dc627e142a3f1eef9c557b62c92
vaheminasyan2 <vaheminasyan2@gmail.com>
2020-02-09 22:16:07 -0800

    exercise 7 of 13

---
+++ http-collect.js
@@ -0,0 +1,7 @@
+// HTTP GET with URL at procces.argv[2]
+// Collect all data from server
+// console.log two lines 
+    // 1st line - integer: # of chars receive from the server
+    // 2nd line - complete string of chars sent by server 
+
+    
